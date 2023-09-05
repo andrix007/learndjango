0ca71af5c788e8dd7bdfec506f443c543d1367a3
Andrei Bancila <andreirbancila@gmail.com>
2023-09-05 09:44:35 +0200

    Removed secret key

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(!v0om*54*7oc!a^)-ho@tbcej(h&7z_-0^#4p4nnrh8621ojk'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
